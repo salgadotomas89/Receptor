56b33eabc25026de711edaf89373e6aec728b208
Tomas Salgado <salgadotomas@outllok.com>
2016-10-26 20:45:32 -0300

    -aes desencriptada -hash desencriptado -texto desencriptado

--- src/Receptor.java
+++ src/Receptor.java
@@ -2,11 +2,13 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.security.Key;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
 import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
@@ -24,10 +26,11 @@ public class Receptor {
         try{                       
             tamaño=inFile.length();
             System.out.println("tamaño del archivo:"+tamaño);
-            byte [] encriptado = new byte[512];//tamaño de la llave aes encriptada en bytes
+            byte [] encriptado = new byte[528];//tamaño de la llave aes encriptada en bytes
             FileInputStream input = new FileInputStream(inFile);
             input.read(encriptado);
             input.close();
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //Carga el keystore
             KeyStore myKeyStore = KeyStore.getInstance("JKS");
             FileInputStream inStream = new FileInputStream(keyStoreFile);
@@ -44,20 +47,30 @@ public class Receptor {
             }
             Cipher rsa = Cipher.getInstance("RSA");
             rsa.init(Cipher.DECRYPT_MODE, privatekey);//incializamos el objeto rsa
-            byte[] bytesDesencriptados = rsa.doFinal(AEs);            
-            String AesDesencriptada = new String(bytesDesencriptados);
-            System.out.println("key desencriptada:"+AesDesencriptada);
-            //hasta aqui funciona bien
-            byte[] Hash= new byte[256];
+            byte[] bytesDesencriptados = rsa.doFinal(AEs);                        
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            byte[] HashEncriptado= new byte[256];
             int h=0;
             for(int i=256;i<512;i++){                
-                  Hash[h++]=encriptado[i];                               
+                  HashEncriptado[h++]=encriptado[i]; 
+               
             }
             Cipher cifradorRsaHash = Cipher.getInstance("RSA");
-            cifradorRsaHash.init(Cipher.ENCRYPT_MODE, publicKey);
-            byte[] HashDesencriptado = cifradorRsaHash.doFinal(Hash);
-            System.out.println("holitas");
-
+            cifradorRsaHash.init(Cipher.DECRYPT_MODE, publicKey);
+            byte[] HashDesencriptado = cifradorRsaHash.doFinal(HashEncriptado);
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            byte[] TextoEncriptado= new byte[16];
+            int t=0;
+            for(int i=512;i<528;i++){                
+                  TextoEncriptado[t++]=encriptado[i];                               
+                  
+            }
+            Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            Key alKey = new SecretKeySpec(bytesDesencriptados, 0, bytesDesencriptados.length, "AES"); 
+            aes.init(Cipher.DECRYPT_MODE,alKey);
+            byte[] TextoDesencriptado = aes.doFinal(TextoEncriptado);
+            // Texto obtenido, igual al original.
+            System.out.println("Texto desencriptado:"+new String(TextoDesencriptado));
             
     
         }catch(Exception e){
